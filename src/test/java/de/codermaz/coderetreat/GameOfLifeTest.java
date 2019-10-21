6e1a178656c80c24d185e931e52ee2dd6c4f1f8b
codermaz <yilmazadem@gmail.com>
2019-10-21 20:37:37 +0200

    Initial commit

---
+++ src/test/java/de/codermaz/coderetreat/GameOfLifeTest.java
@@ -0,0 +1,19 @@
+package de.codermaz.coderetreat;
+
+import org.junit.After;
+import org.junit.Before;
+
+
+public class GameOfLifeTest
+{
+
+	@Before public void setUp() throws Exception
+	{
+
+	}
+
+	@After public void tearDown() throws Exception
+	{
+	}
+
+}
